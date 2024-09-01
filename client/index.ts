851f1775ca03f7145e012664b82c994218dd0670
ducklol2 <140205571+ducklol2@users.noreply.github.com>
2024-09-01 22:16:16 +0000

    happy follows path

--- client/index.ts
+++ client/index.ts
@@ -7,13 +7,94 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const context = canvas.getContext('2d');
-context.beginPath();
 
-context.arc(75.0, 75.0, 50.0, 0.0, Math.PI * 2);
-context.moveTo(110.0, 75.0);
-context.arc(75.0, 75.0, 35.0, 0.0, Math.PI);
-context.moveTo(65.0, 65.0);
-context.arc(60.0, 65.0, 5.0, 0.0, Math.PI * 2);
-context.moveTo(95.0, 65.0);
-context.arc(90.0, 65.0, 5.0, 0.0, Math.PI * 2);
-context.stroke();
+type Point = {
+    x: number;
+    y: number;
+}
+
+type Path = {
+    points: Point[];
+}
+
+let x = window.innerWidth / 2;
+let y = window.innerHeight / 2;
+let r = 50;
+let rSq = 1;
+
+const path: Path = { points: [] };
+window.addEventListener('mousemove', ev => {
+    if (ev.buttons) {
+        path.points.push({ x: ev.clientX, y: ev.clientY });
+    }
+});
+
+setInterval(loop, 50);
+function loop() {
+    move();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    draw();
+    // if (r === 50) {
+    //     rSq = -1;
+    // }
+    // if (r === 10) {
+    //     rSq = 1;
+    // }
+    // r += rSq;
+}
+
+function move() {
+    if (path.points.length == 0) return;
+
+    if (Math.abs(x - path.points[0].x) < 10
+     && Math.abs(y - path.points[0].y) < 10) {
+        path.points.shift();
+    }
+
+    if (path.points.length == 0) return;
+
+    const dX = path.points[0].x - x;
+    const dY = path.points[0].y - y;
+
+    x += Math.sign(dX) * Math.min(5, Math.abs(dX));
+    y += Math.sign(dY) * Math.min(5, Math.abs(dY));
+}
+
+function draw() {
+    drawFace(x, y, r);
+    drawPath();
+}
+
+function drawPath() {
+    context.beginPath();
+
+    let pathX: number;
+    let pathY: number;
+    for ({ x: pathX, y: pathY } of path.points) {
+        context.arc(pathX, pathY, 2, 0, Math.PI * 2);
+    }
+
+    context.stroke();
+}
+
+function drawFace(x: number, y: number, r: number) {
+    context.beginPath();
+    // r = 50;
+    // outside circle head
+    // 75, 75, 50
+    context.arc(x, y, r, 0.0, Math.PI * 2);
+
+    // mouth
+    context.moveTo(x + 0.7 * r, y);
+    context.arc(x, y, r * 0.7, 0.0, Math.PI);
+
+    // left eye
+    context.moveTo(x - r * 0.2, y - r * 0.2);
+    context.arc(x - r * 0.3, y - r * 0.2, r * 0.1, 0.0, Math.PI * 2);
+
+    // right eye
+    context.moveTo(x + r * 0.4, y - r * 0.2);
+    context.arc(x + r * 0.3, y - r * 0.2, r * 0.1, 0, Math.PI * 2);
+
+    context.stroke();
+}
