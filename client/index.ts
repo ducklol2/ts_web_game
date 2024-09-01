37da2ce58be630a58db15e7956e549f60635144f
ducklol2 <140205571+ducklol2@users.noreply.github.com>
2024-09-01 21:16:16 +0000

    face is back

--- client/index.ts
+++ client/index.ts
@@ -1,3 +1,19 @@
 
 
 console.log('hi');
+
+const canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext('2d');
+context.beginPath();
+
+context.arc(75.0, 75.0, 50.0, 0.0, Math.PI * 2);
+context.moveTo(110.0, 75.0);
+context.arc(75.0, 75.0, 35.0, 0.0, Math.PI);
+context.moveTo(65.0, 65.0);
+context.arc(60.0, 65.0, 5.0, 0.0, Math.PI * 2);
+context.moveTo(95.0, 65.0);
+context.arc(90.0, 65.0, 5.0, 0.0, Math.PI * 2);
+context.stroke();
