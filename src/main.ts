9d1977c3700c3ce06ab87023f3b6e1405e850488
ducklol2 <140205571+ducklol2@users.noreply.github.com>
2024-09-02 20:21:34 +0000

    fill the eyes

--- src/main.ts
+++ src/main.ts
@@ -213,14 +213,17 @@ function drawFace(x: number, y: number, r: number = 50) {
     // mouth
     context.moveTo(x + 0.7 * r, y);
     context.arc(x, y, r * 0.7, 0.0, Math.PI);
+    context.stroke();
 
     // left eye
+    context.fillStyle = 'black';
+    context.beginPath();
     context.moveTo(x - r * 0.2, y - r * 0.2);
     context.arc(x - r * 0.3, y - r * 0.2, r * 0.1, 0.0, Math.PI * 2);
 
     // right eye
     context.moveTo(x + r * 0.4, y - r * 0.2);
     context.arc(x + r * 0.3, y - r * 0.2, r * 0.1, 0, Math.PI * 2);
+    context.fill();
 
-    context.stroke();
 }
